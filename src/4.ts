d2a9ef76c450806483cb72f8b1e62951680be463
Alexander <alexkorpol1964@gmail.com>
2023-10-31 17:47:41 +0200

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,30 @@
+class Key{
+    private signature: number = (Math.floor(Math.random() * 100) + 1);
+    // this.signature = Math.random();
+
+    getSignature() {
+        return this.signature
+    }
+
+}
+abstract class House{
+    protected door: boolean = false;
+    tenants = []
+    key = new Key;
+    public abstract OpenDoor(key):void
+    comeIn() {
+        if (this.door) {
+           this.tenants.push( new Person)  
+        }
+    }
+}
+
+class MyHouse extends House{
+
+}
+class Person{
+
+}
 const key = new Key();
 
 const house = new MyHouse(key);
